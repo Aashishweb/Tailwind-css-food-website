8d8dfde168cddaa910f7788aa3e33ee7b7900172
Ashish <102414529+Aashishweb@users.noreply.github.com>
2022-10-31 10:20:31 +0530

    changes path for css file

--- tailwind-default.config.js
+++ tailwind-default.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['public/{*.html}'],
+  content: ['{index.html}'],
   presets: [],
   darkMode: 'media', // or 'class'
   theme: {
